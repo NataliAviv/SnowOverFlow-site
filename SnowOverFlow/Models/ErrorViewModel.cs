692cfa14bda17b3755c0524f9869337f639defb4
NataliAviv <natali.aviv162@gmail.com>
2018-09-15 19:46:42 +0300

    Add project files.

---
+++ SnowOverFlow/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SnowOverFlow.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
