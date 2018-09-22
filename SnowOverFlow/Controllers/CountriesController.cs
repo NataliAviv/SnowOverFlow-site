40b8e689b1d92ac87eaf5ac1f65f48f5aea33706
eilonv3 <eilonv3@gmail.com>
2018-09-22 20:00:33 +0300

    eilon tests

--- SnowOverFlow/Controllers/CountriesController.cs
+++ SnowOverFlow/Controllers/CountriesController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using SnowOverFlow.Data;
-using SnowOverFlow.Models;
+using SnowOverFlow.Models;dsadasdasd
 
 namespace SnowOverFlow
 {
