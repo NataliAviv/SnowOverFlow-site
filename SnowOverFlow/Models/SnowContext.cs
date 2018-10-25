65df4e7cdfa14276875b5c5c6aea21be6e61c00e
Natali_Aviv <Natali_Aviv@DESKTOP-KV7HN0O>
2018-10-25 11:00:32 +0300

    new change on snowcontext

--- SnowOverFlow/Models/SnowContext.cs
+++ SnowOverFlow/Models/SnowContext.cs
@@ -1,24 +1,34 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Web;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SnowOverFlow.Models;
 
-namespace SnowOverFlow.Models
+namespace SnowOverFlow.Data
 {
-    public class SnowContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public SnowContext() 
-            : base()
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
         {
         }
-        public System.Data.Entity.DbSet<SnowOverFlow.Models.Continent> Continents { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
 
-        public System.Data.Entity.DbSet<SnowOverFlow.Models.Country> Countries { get; set; }
+        public DbSet<SnowOverFlow.Models.Country> Country { get; set; }
 
-        public System.Data.Entity.DbSet<SnowOverFlow.Models.Site> Sites { get; set; }
+        public DbSet<SnowOverFlow.Models.Site> Site { get; set; }
 
+        public DbSet<SnowOverFlow.Models.Continent> Continent { get; set; }
+
+        public DbSet<SnowOverFlow.Models.User> User { get; set; }
     }
 }
