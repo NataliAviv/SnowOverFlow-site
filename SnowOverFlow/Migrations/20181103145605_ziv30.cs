4440e5636d848d65281a4a4a137ff809a75de7cb
Ziv Cohen <ziv.cohen.3456@gmail.com>
2018-11-03 17:03:30 +0200

    try

---
+++ SnowOverFlow/Migrations/20181103145605_ziv30.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace SnowOverFlow.Migrations
+{
+    public partial class ziv30 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
