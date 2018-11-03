f3c63ad6828145cdbaddd9a47ec9f931619d6333
Natali_Aviv <Natali_Aviv@DESKTOP-KV7HN0O>
2018-11-03 18:50:16 +0200

    search opttions

---
+++ SnowOverFlow/Migrations/20181103125427_data.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace SnowOverFlow.Migrations
+{
+    public partial class data : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- SnowOverFlow/Models/SnowContext.cs
+++ SnowOverFlow/Models/SnowContext.cs
@@ -15,6 +15,10 @@ namespace SnowOverFlow.Data
         {
         }
 
+        public ApplicationDbContext()
+        {
+        }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
