d0f3db271656300d9a998d4ee070ef574e730cd4
eilonv3 <eilonv3@gmail.com>
2018-11-03 14:13:39 +0200

    made some changes, so: 1. only admin can see edit/delete buttons 2. only admin can commit the two mentioned above.

---
+++ SnowOverFlow/Migrations/20181103121052_eilon3.11.2018.1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace SnowOverFlow.Migrations
+{
+    public partial class eilon31120181 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
